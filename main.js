acfd88aacdbc1144865cbf2a53246b7d08b6e14b
skrtsman <gamisou10@gmail.com>
2022-09-10 04:17:07 +0300

    Logic Refactoring

--- main.js
+++ main.js
@@ -1,4 +1,25 @@
+const topOutput = document.querySelector(".upper-right");
+const botOutput = document.querySelector(".lower-right");
+const buttons = document.querySelectorAll("button");
+const clearBtn = document.querySelector(".clear");
+const symbols = ["/","+","-","=","*","AC","DEL"]
+const buttonsArr = Array.from(buttons);
+let nextDisplayValue;
 let displayValue;
+let count = 0;
+
+buttonsArr.map(btn => {
+   btn.addEventListener("click",()=>{
+      if(symbols.includes(btn.textContent)){
+         botOutput.textContent += btn.textContent
+         // console.log(operate(displayValue,nextDisplayValue(),btn.textContent))
+      }else{
+         botOutput.textContent += btn.textContent
+         displayValue = botOutput.textContent
+      }
+      console.log(displayValue);
+   }) 
+})
 
 function add(a,b){return a + b;}
 function divide(a,b){return a / b;}
