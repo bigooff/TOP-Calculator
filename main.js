4fc4567d2898f0d9c2462fce7125d706e8026758
skrtsman <gamisou10@gmail.com>
2022-09-13 02:40:00 +0300

    Complete JS logic refactoring

--- main.js
+++ main.js
@@ -1,25 +1,49 @@
 const topOutput = document.querySelector(".upper-right");
 const botOutput = document.querySelector(".lower-right");
-const buttons = document.querySelectorAll("button");
+const numberBtns = document.querySelectorAll(".number");
+const operationBtns = document.querySelectorAll(".operation");
+const equalsBtn = document.querySelector("#equals");
+const deleteBtn = document.querySelector(".delete");
 const clearBtn = document.querySelector(".clear");
-const symbols = ["/","+","-","=","*","AC","DEL"]
-const buttonsArr = Array.from(buttons);
-let nextDisplayValue;
+const numArray = Array.from(numberBtns);
+const opsArray = Array.from(operationBtns);
+// const currentOperand = botOutput.textContent;
+// const prevOperand = topOutput.textContent;
 let displayValue;
-let count = 0;
 
-buttonsArr.map(btn => {
-   btn.addEventListener("click",()=>{
-      if(symbols.includes(btn.textContent)){
-         botOutput.textContent += btn.textContent
-         // console.log(operate(displayValue,nextDisplayValue(),btn.textContent))
-      }else{
-         botOutput.textContent += btn.textContent
-         displayValue = botOutput.textContent
-      }
+deleteBtn.addEventListener("click", ()=>{
+   botOutput.textContent = botOutput.textContent.slice(1,-1);
+});
+clearBtn.addEventListener("click", ()=>{
+   topOutput.textContent = "";
+   botOutput.textContent = "";
+});
+
+numArray.forEach((num)=>{
+   num.addEventListener("click",()=>{
+      if(num.textContent === "." && botOutput.textContent.includes(".")) { return; }
+      displayValue = num.textContent
+      botOutput.textContent += displayValue;
       console.log(displayValue);
-   }) 
+
+   })
+});
+
+opsArray.forEach((operation) => {
+   
 })
+
+equalsBtn.addEventListener("click", ()=>{
+   let currentOperand = parseInt(botOutput.textContent);
+   let previousOperand = parseInt(topOutput.textContent);
+   
+   if(operation === "/"){
+      divide(previousOperand,currentOperand);
+   }
+})
+
+
+
 
 function add(a,b){return a + b;}
 function divide(a,b){return a / b;}
