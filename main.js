c2085374ac6114667293a1823ca9f021239f838d
skrtsman <gamisou10@gmail.com>
2022-09-13 19:27:03 +0300

    Actually finished project

--- main.js
+++ main.js
@@ -1,5 +1,4 @@
-//TODO: Add Error for divide by 0
-//Add operation chaining example: 12 * 2 - 2 = 22;
+
 const topOutput = document.querySelector(".upper-right");
 const botOutput = document.querySelector(".lower-right");
 const numberBtns = document.querySelectorAll(".number");
@@ -9,16 +8,22 @@ const deleteBtn = document.querySelector(".delete");
 const clearBtn = document.querySelector(".clear");
 const numArray = Array.from(numberBtns);
 const opsArray = Array.from(operationBtns);
-
+let operationChain = [];
 let displayValue;
+
 
 deleteBtn.addEventListener("click", ()=>{
    botOutput.textContent = botOutput.textContent.slice(0 , -1)
 });
+
+
 clearBtn.addEventListener("click", ()=>{
+   operationChain = [];
    topOutput.textContent = "";
    botOutput.textContent = "";
 });
+
+
 numArray.forEach((num)=>{
    num.addEventListener("click",()=>{
       if(num.textContent === "." && botOutput.textContent.includes(".")) { return; }
@@ -27,40 +32,56 @@ numArray.forEach((num)=>{
       // console.log(displayValue);
    })
 });
+
+
+let opResult;
 let chosenOperation;
+let prevOperation;
 opsArray.forEach((operation) => {
    operation.addEventListener("click", ()=>{
+      operationChain.push(chosenOperation)
       if(botOutput.textContent === ""){return;}
       chosenOperation = operation.textContent;
-      if(topOutput.textContent.includes(chosenOperation) && botOutput.textContent !== ""){
+      
+      if(topOutput.textContent !== ""){
+         prevOperation = operationChain[operationChain.length - 1]
+         // console.log(prevOperation);
+         // console.log(operationChain);
          currentOperand = parseFloat(botOutput.textContent);
          previousOperand = parseFloat(topOutput.textContent);
-         result = operate(previousOperand,currentOperand,chosenOperation)
-         update();
+         opResult = operate(previousOperand,currentOperand,prevOperation)       
+         topOutput.textContent = opResult + " " + chosenOperation;
+         botOutput.textContent = "";
+         return;
       }
+      opResult = operate(previousOperand,currentOperand,chosenOperation) 
       topOutput.textContent = botOutput.textContent + " " + operation.textContent;
       botOutput.textContent = "";
-      console.log(chosenOperation);
+      return;
+      // console.log(chosenOperation);
    })
 })
+
+
 let previousOperand;
 let currentOperand;
-let result;
 equalsBtn.addEventListener("click", ()=>{
+   let result;
     currentOperand = parseFloat(botOutput.textContent);
     previousOperand = parseFloat(topOutput.textContent);
-   if(topOutput.textContent === ""){return;}
+   if(topOutput.textContent === "" || isNaN(currentOperand) || isNaN(previousOperand)){return;}
    result = operate(previousOperand,currentOperand,chosenOperation)
-   update();
-})
-function update(){
    topOutput.textContent = "";
    botOutput.textContent = result;
-}
+})
+
+
+
 function add(a,b){return a + b;}
 function divide(a,b){return a / b;}
 function multiply(a,b){return a * b;}
 function subtract(a,b){return a - b;}
+
 
 function operate(firstNum,secondNum,operator){
     if(operator === "+"){
@@ -70,6 +91,7 @@ function operate(firstNum,secondNum,operator){
     }else if(operator === "*"){
        return multiply(firstNum,secondNum);
     }else if(operator === "/"){
+       if(firstNum === 0 || secondNum === 0){return "Division by 0 is prohibited"}
        return divide(firstNum,secondNum)
     }
 }
