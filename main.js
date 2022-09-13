b28c423f05c342f0a37ded5eed40ee4d4f480df3
skrtsman <gamisou10@gmail.com>
2022-09-13 03:11:34 +0300

    Finished TOP Calculator Project

--- main.js
+++ main.js
@@ -7,43 +7,44 @@ const deleteBtn = document.querySelector(".delete");
 const clearBtn = document.querySelector(".clear");
 const numArray = Array.from(numberBtns);
 const opsArray = Array.from(operationBtns);
-// const currentOperand = botOutput.textContent;
-// const prevOperand = topOutput.textContent;
 let displayValue;
 
 deleteBtn.addEventListener("click", ()=>{
-   botOutput.textContent = botOutput.textContent.slice(1,-1);
+   botOutput.textContent = botOutput.textContent.slice(0 , -1)
 });
 clearBtn.addEventListener("click", ()=>{
    topOutput.textContent = "";
    botOutput.textContent = "";
 });
-
 numArray.forEach((num)=>{
    num.addEventListener("click",()=>{
       if(num.textContent === "." && botOutput.textContent.includes(".")) { return; }
       displayValue = num.textContent
       botOutput.textContent += displayValue;
-      console.log(displayValue);
-
+      // console.log(displayValue);
    })
 });
 
 opsArray.forEach((operation) => {
-   
+   operation.addEventListener("click", ()=>{
+      if(botOutput.textContent === ""){return;}
+      topOutput.textContent = botOutput.textContent + " " + operation.textContent;
+      botOutput.textContent = "";
+      chosenOperation = operation.textContent;
+      // console.log(chosenOperation);
+   })
 })
 
+
+let chosenOperation;
 equalsBtn.addEventListener("click", ()=>{
-   let currentOperand = parseInt(botOutput.textContent);
-   let previousOperand = parseInt(topOutput.textContent);
-   
-   if(operation === "/"){
-      divide(previousOperand,currentOperand);
-   }
+   if(topOutput.textContent === ""){return;}
+   let currentOperand = parseFloat(botOutput.textContent);
+   let previousOperand = parseFloat(topOutput.textContent);
+   let result = operate(previousOperand,currentOperand,chosenOperation)
+   topOutput.textContent = "";
+   botOutput.textContent = result;
 })
-
-
-
 
 function add(a,b){return a + b;}
 function divide(a,b){return a / b;}
